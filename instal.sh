a1410cf37f5dcff8a5d780928e3f03296a586383
Tiago <tiagopn@Jarvis2.0>
2023-01-17 11:18:44 -0300

    install

---
+++ instal.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+# get the path to this script
+MY_PATH=`dirname "$0"`
+MY_PATH=`( cd "$MY_PATH" && pwd )`
+
+# define paths
+APPCONFIG_PATH=$MY_PATH/appconfig
+
+# install packages
+sudo apt-get -y update
