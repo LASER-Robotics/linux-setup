31cbef46488b3fb816eb282b8ca9ebfae8d5fd54
Tiago <tiagopn@Jarvis2.0>
2023-01-19 18:56:23 -0300

    finalizado 1

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# linux-setup
-Tiago's Linux environment
+# klaxalk's Linux environment
+
+| Ubuntu               | Architecture | Status                                                                                                                                                           |
+| -------------------- | ------------ | ---------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| 20.04 Focal          | AMD64        | [![Build Status](https://github.com/klaxalk/linux-setup/workflows/Focal/badge.svg)](https://github.com/klaxalk/linux-setup/actions)                              |
+| 20.04 Focal          | arm64        |                                                                                                                                                                  |
+
+## Summary
+
+This repo contains settings of klaxalk's Linux work environment.
+
+It could be summarized as follows:
+* **Ubuntu 20.04**
+* **i3** (i3gaps) tiling window manager with i3bar and vim-like controls
+  * **i3-layout-manager** for saving and loading window layouts
+* **urxvt** terminal emulator with ability to show images (when using the *ranger* file manager)
+* **tmux** terminal multiplexer is running all the time
+  * **tmuxinator** for automation of tmux session
+  * vim-compatible key bindings for split switching
+* **vim** is everywhere
+  * pluginized for smooth c++ and ROS development
+  * youcompleteme
+  * UltiSnips
+  * shared clipboards between vim, tmux and OS
+  * Ctrl+P
+  * smooth latex development with vimtex and zathura
+  * Tim Pope is the king
+* **athame** gives you vim in the terminal (zsh)
+  * handfull of plugins in terminal: ultisnips, vim-surround, targets.vim, vim-exchange, etc.
+* **zsh** better shell for everyday use
+* **ranger** terminal file manager
+* **profile_manager** and **epigen** for switching between machine-specific configurations (profiles within dotfiles)
+  * all-in-one configuration, no git branching, no more cherrypicking
+  * sharing configs between multiple users
+  * sharing configs between different machines
+  * seamless switching of colorschemes
+
+To clone and install everything run following code. **BEWARE**, running this will **DELETE** your current .i3, tmux, vim, etc. dotfiles.
+```bash
+cd /tmp
+echo "mkdir -p ~/git
+cd ~/git
+sudo apt-get -y install git
+git clone https://github.com/klaxalk/linux-setup.git
+cd linux-setup
+./install.sh" > run.sh && source run.sh
+```
+**Calling install.sh repeatedly** will not cause acumulation of code in your .bashrc, so feel free to update your configuration by rerunning it.
+
+# How to? -> [wiki](https://github.com/klaxalk/linux-setup/wiki)
+
+Refer to the project's [wiki](https://github.com/klaxalk/linux-setup/wiki) (work in progress).
+
+# Credits
+
+I thank the following sources for inspiration:
+
+* All guys behind [thoughtbot](https://www.youtube.com/user/ThoughtbotVideo) and namely following presenters:
+  * Mike Coutermarsh, https://www.youtube.com/watch?v=_NUO4JEtkDw
+  * Chris Toomey, https://www.youtube.com/watch?v=wlR5gYd6um0
+  * Aaron Bieber, https://www.youtube.com/watch?v=JWD1Fpdd4Pc
+* Nick Nisi, https://www.youtube.com/channel/UCbNhLf99gKKXdXm0aFfQFKw
+* Luke Smith, https://www.youtube.com/channel/UC2eYFnH61tmytImy1mTYvhA
+* Alex Booker, https://www.youtube.com/watch?v=_kjbj-Ez1vU
+* Chris Hunt, https://www.youtube.com/watch?v=9jzWDr24UHQ
+* Gaël Ecorchard, https://github.com/galou
+
+# Troubleshooting
+
+It is possible and probable that after you update using ```git pull```, something might not work anymore.
+This usually happens due to new programs, plugins, and dependencies that might not be satisfied anymore.
+I suggest re-running **install.sh**, after each update.
+
+# Disclaimer
+
+This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
--- instal.sh
+++ instal.sh
@@ -13,4 +13,224 @@ MY_PATH=`( cd "$MY_PATH" && pwd )`
 APPCONFIG_PATH=$MY_PATH/appconfig
 
 # install packages
-sudo apt-get -y update
+sudo apt update
+
+subinstall_params=""
+unattended=0
+for param in "$@"
+do
+  echo $param
+  if [ $param="--unattended" ]; then
+    echo "installing in unattended mode"
+    unattended=1
+    subinstall_params="--unattended"
+  fi
+done
+
+cd $MY_PATH
+git submodule update --init --recursive
+
+arch=`uname -i`
+
+# essentials
+sudo apt install git tig cmake cmake-curses-gui build-essential automake autoconf autogen libncurses5-dev libc++-dev pkg-config libtool net-tools openssh-server nmap
+
+# other stuff
+sudo apt install ruby sl indicator-multiload figlet toilet gem tree exuberant-ctags xclip xsel exfat-fuse exfat-utils blueman autossh jq xvfb gparted espeak ncdu
+
+# python
+sudo apt install python3-serial python3-dev python-setuptools python3-setuptools python3-pip
+
+sudo apt install python3-git
+
+
+if [ "$unattended" == "0" ]
+then
+  if [ "$?" != "0" ]; then echo "Press Enter to continues.." && read; fi
+fi
+
+# install TMUX
+bash $APPCONFIG_PATH/tmux/install.sh $subinstall_params
+
+# setup RANGER
+bash $APPCONFIG_PATH/ranger/install.sh $subinstall_params
+
+# install VIM
+bash $APPCONFIG_PATH/vim/install.sh $subinstall_params
+
+# install HTOP-VIM
+bash $APPCONFIG_PATH/htop-vim/install.sh $subinstall_params
+
+# install URXVT
+bash $APPCONFIG_PATH/urxvt/install.sh $subinstall_params
+
+# install ZSH with ATHAME
+bash $APPCONFIG_PATH/zsh/install.sh $subinstall_params
+
+# install I3
+bash $APPCONFIG_PATH/i3/install.sh $subinstall_params
+
+# install LATEX and PDF support
+bash $APPCONFIG_PATH/latex_pdf/install.sh $subinstall_params
+
+# install MULTIMEDIA support
+bash $APPCONFIG_PATH/multimedia/install.sh $subinstall_params
+
+# install SHUTTER
+if [ "$arch" != "aarch64" ]; then
+  bash $APPCONFIG_PATH/shutter/install.sh $subinstall_params
+fi
+
+# install ZATHURA
+bash $APPCONFIG_PATH/zathura/install.sh $subinstall_params
+
+# install VIMIV
+bash $APPCONFIG_PATH/vimiv/install.sh $subinstall_params
+
+# setup modified keyboard rules
+bash $APPCONFIG_PATH/keyboard/install.sh $subinstall_params
+
+# setup fuzzyfinder
+bash $APPCONFIG_PATH/fzf/install.sh $subinstall_params
+
+# install VIM-STREAM
+bash $APPCONFIG_PATH/vim-stream/install.sh $subinstall_params
+
+# install GRUB CUSTOMIZER
+if [ "$arch" != "aarch64" ]; then
+  bash $APPCONFIG_PATH/grub-customizer/install.sh $subinstall_params
+fi
+
+# install TMUXINATOR
+bash $APPCONFIG_PATH/tmuxinator/install.sh $subinstall_params
+
+
+
+
+
+#############################################
+# remove the interactivity check from bashrc
+#############################################
+
+VIM_BIN="$(whereis vim | awk '{print $2}')"
+HEADLESS=""
+
+# this caused some problems once, but where?
+$VIM_BIN $HEADLESS -E -s -c "%g/running interactively/norm dap" -c "wqa" -- ~/.bashrc
+
+
+
+
+
+#############################################
+# adding GIT_PATH variable to .bashrc
+#############################################
+
+# add variable for path to the git repository
+num=`cat ~/.bashrc | grep "GIT_PATH" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  TEMP=`( cd "$MY_PATH/../" && pwd )`
+
+  echo "Adding GIT_PATH variable to .bashrc"
+  # set bashrc
+  echo "
+# path to the git root
+export GIT_PATH=$TEMP" >> ~/.bashrc
+fi
+
+
+
+
+
+##################################################
+# install inputs libraries when they are missing
+##################################################
+sudo apt -y install xserver-xorg-input-all
+
+
+
+
+
+#############################################
+# Disable automatic update over apt
+#############################################
+
+sudo systemctl disable apt-daily.service
+sudo systemctl disable apt-daily.timer
+
+sudo systemctl disable apt-daily-upgrade.timer
+sudo systemctl disable apt-daily-upgrade.service
+
+
+
+
+
+#############################################
+# link the scripts folder
+#############################################
+
+if [ ! -e ~/.scripts ]; then
+  ln -sf $MY_PATH/scripts ~/.scripts
+fi
+
+
+
+
+
+#############################################
+# add PROFILES variables
+#############################################
+
+num=`cat ~/.bashrc | grep "PROFILES" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  echo "Adding epigen rules to .bashrc"
+  echo '
+# list (space-separated) of profile names for customizing configs
+export PROFILES="COLORSCHEME_DARK"' >> ~/.bashrc
+
+fi
+
+
+
+
+
+#############################################
+# fix touchpad touch-clicking
+#############################################
+
+if [ ! -e /etc/X11/xorg.conf.d/90-touchpad.conf ]; then
+  $MY_PATH/scripts/fix_touchpad_click.sh
+fi
+
+
+
+
+
+#############################################
+# add sourcing of dotbashrd to .bashrc
+#############################################
+num=`cat ~/.bashrc | grep "dotbashrc" | wc -l`
+if [ "$num" -lt "1" ]; then
+
+  echo "Adding source to .bashrc"
+  # set bashrc
+  echo "
+# sourcing Tiago's linux setup
+source $APPCONFIG_PATH/bash/dotbashrc" >> ~/.bashrc
+
+fi
+
+
+
+
+
+# deploy configs by Profile manager
+./deploy_configs.sh
+
+# finally source the correct rc file
+toilet All Done
+
+# say some tips to the new user
+echo "Hurray, the 'Linux Setup' should be ready, try opening a new terminal."
