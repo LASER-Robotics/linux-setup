644c9bda69321501708e1628cd48c470a09d6fef
Tiago <tiagopn@Jarvis2.0>
2023-01-20 13:42:57 -0300

    finished 11b

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -42,24 +42,22 @@ while true; do
 
     sudo -H pip3 install rospkg
 
-    # # compile vim from sources
-    # cd $APP_PATH/../../submodules/vim
-    # ./configure --with-features=huge \
-    #   --enable-multibyte \
-    #   --enable-python3interp=yes \
-    #   --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
-    #   --enable-perlinterp=yes \
-    #   --enable-luainterp=yes \
-    #   --enable-gui=no \
-    #   --enable-cscope --prefix=/usr
+    # compile vim from sources
+    cd $APP_PATH/../../submodules/vim
+    ./configure --with-features=huge \
+      --enable-multibyte \
+      --enable-python3interp=yes \
+      --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+      --enable-perlinterp=yes \
+      --enable-luainterp=yes \
+      --enable-gui=no \
+      --enable-cscope --prefix=/usr
 
-    #   cd src
-    #   make
-    #   cd ../
-    #   make VIMRUNTIMEDIR=/usr/share/vim/vim81
-    #   sudo make install
-
-    sudo apt -y install vim
+      cd src
+      make
+      cd ../ 
+      make VIMRUNTIMEDIR=/usr/share/vim/vim81
+      sudo make install
 
     # set vim as a default git mergetool
     git config --global merge.tool vimdiff
