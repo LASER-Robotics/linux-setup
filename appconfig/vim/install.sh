db42bc4bb5dce8ac854cd53562326f825037cfc0
Tiago <tiagopn@Jarvis2.0>
2023-01-20 21:48:49 -0300

    new vim

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -38,7 +38,13 @@ while true; do
 
     sudo apt-get -y remove vim-* || echo ""   #remove olde vim
 
+    # # Tiago
+    # sudo apt-get install vim
+
     sudo apt-get -y install libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python3-dev clang-format
+
+    # Tiago
+    sudo apt-get -y install  clang libgtk3.0-cil-dev
 
     sudo -H pip3 install rospkg
 
@@ -47,7 +53,7 @@ while true; do
     ./configure --with-features=huge \
       --enable-multibyte \
       --enable-python3interp=yes \
-      --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
+      --with-python3-config-dir=/usr/lib/python3.10/config-3.10-x86_64-linux-gnu \
       --enable-perlinterp=yes \
       --enable-luainterp=yes \
       --enable-gui=no \
@@ -56,7 +62,7 @@ while true; do
       cd src
       make
       cd ../ 
-      make VIMRUNTIMEDIR=/usr/share/vim/vim81
+      make VIMRUNTIMEDIR=/usr/share/vim/vim90
       sudo make install
 
     # set vim as a default git mergetool
@@ -90,13 +96,13 @@ while true; do
         sudo apt -y install mono-complete golang nodejs openjdk-17-jdk openjdk-17-jre
 
         # if 22.04, just install python3-clang from apt
-        sudo apt -y install python3-clang 
+        sudo apt -y install python3-clang python3-clang-14
         
         # install prequisites for YCM
-        sudo apt -y install clangd-11
+        sudo apt -y install clangd-14
 
         # set clangd to version 11 by default
-        sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-11 999
+        sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-14 999
         sudo apt -y install libboost-all-dev
 
         cd ~/.vim/plugged/YouCompleteMe/
