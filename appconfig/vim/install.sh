ee327aa9025057b27ac7f52646600e5e7617f62d
Tiago <tiagopn@Jarvis2.0>
2023-01-23 20:07:25 -0300

    vim updated 2

--- appconfig/vim/install.sh
+++ appconfig/vim/install.sh
@@ -45,7 +45,7 @@ while true; do
 
     sudo apt-get install vim
 
-    # curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
     # set vim as a default git mergetool
     git config --global merge.tool vimdiff
